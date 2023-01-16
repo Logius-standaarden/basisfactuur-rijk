713892aa94ccadff8a6e1bd2f11daf754c18e822
Maarten van Bree <63168360+mvb-nerdzone@users.noreply.github.com>
2023-01-16 16:28:50 +0100

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,40 @@
+var respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  license: "cc0",
+  specStatus: "WV",
+  specType: "HR",
+  pubDomain: "dk",
+  shortName: "template",
+  publishDate: "2022-06-24",
+  publishVersion: "0.0.1",
+  title: "titleText",
+  // previousPublishVersion: "(none)",
+  content: {"ch01": "informative", "ch02": "", "mermaid": ""},
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/ReSpec-template",
+
+
+  // Create PDF and link to file in header:
+  // alternateFormats: [
+  //     {
+  //         label: "pdf",
+  //         uri: "rename-me.pdf",
+  //     },
+  // ],
+};
