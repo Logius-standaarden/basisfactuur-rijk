3dba80402ba754320af16bc725ad85b96978a26c
Maarten van Bree <63168360+mvb-nerdzone@users.noreply.github.com>
2023-03-21 21:10:42 +0100

    Ander testje met previous-dingen

--- js/config.js
+++ js/config.js
@@ -13,9 +13,9 @@ var respecConfig = {
   publishDate: "2022-12-13",
   publishVersion: "december 2022",
   title: "Handreiking basisfactuur Rijk",
-  // previousPublishVersion: "Geen",
-  // previousPublishDate: "1970-01-01",
-  // previousMaturity: "WV",
+  previousPublishVersion: "0.0.1",
+  previousPublishDate: "1970-01-01",
+  previousMaturity: "BP",
   content:
       {
           "handreiking-basisfactuur-rijk": "",
